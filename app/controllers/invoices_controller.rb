5316643020dd5a5263f94b6438624e366ffbcfbe
John Tawiah <johnbaktawiah@gmail.com>
2020-09-23 09:28:15 +0000

    Completed summation

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,13 +1,12 @@
 class InvoicesController < ApplicationController
+
     def index
         @project = Project.distinct.pluck(:company)
     end
 
     def show
-        # @timesheet = Timesheet.find(params[:id])
-        # @project = Project.find(params[:id])
         @client = Timesheet.includes(:projects).where(projects: {company: params[:id]})
         @project = Project.joins(:timesheet).where(projects: {company:params[:id]})
-
     end
+    
 end
